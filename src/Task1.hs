fe6e7286348186edd1e10306c273b206a32befe3
Rytis Cepulis <cepRytis@gmail.com>
2020-09-27 01:25:41 +0300

    testing how functions work

--- src/Task1.hs
+++ src/Task1.hs
@@ -1,11 +1,20 @@
 import Task1Message
 
-parse :: Int    -- ^ Size of the matrix (number of columns or rows)
-      -> String -- ^ Encoded message
-      -> From   -- ^ Parsed data structure
-parse _ _ = error "Not implemented"
+-- parse :: Int    -- ^ Size of the matrix (number of columns or rows)
+--       -> String -- ^ Encoded message
+--       -> From   -- ^ Parsed data structure
+-- parse _ _ = error "Not implemented"
 
-convert :: Int  -- ^ Size of the matrix (number of columns or rows)
-        -> From -- ^ Parsed matrix
-        -> To   -- ^ Converted matrix
-convert _ _ = error "Not implemented"
+-- convert :: Int  -- ^ Size of the matrix (number of columns or rows)
+--         -> From -- ^ Parsed matrix
+--         -> To   -- ^ Converted matrix
+-- convert _ _ = error "Not implemented"
+
+
+
+
+-- test :: Integer -> Integer
+-- test a = a + 5
+
+-- addOne :: Integer -> Integer
+-- addOne a = a + 1
