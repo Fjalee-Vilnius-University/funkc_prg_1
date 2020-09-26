7f8ee3942d86e5a2cf2dcf00faf6a11702bbbe7a
Rytis Cepulis <cepRytis@gmail.com>
2020-09-26 22:00:44 +0300

    recreating example during lecture

---
+++ src/Task1.hs
@@ -0,0 +1,11 @@
+import Task1Message
+
+parse :: Int    -- ^ Size of the matrix (number of columns or rows)
+      -> String -- ^ Encoded message
+      -> From   -- ^ Parsed data structure
+parse _ _ = error "Not implemented"
+
+convert :: Int  -- ^ Size of the matrix (number of columns or rows)
+        -> From -- ^ Parsed matrix
+        -> To   -- ^ Converted matrix
+convert _ _ = error "Not implemented"
