a7a5fbdae7c6da848cc433271d7b42de880b76cd
Rytis Cepulis <cepRytis@gmail.com>
2020-09-27 22:58:36 +0300

    removing trash comments

--- src/Task1.hs
+++ src/Task1.hs
@@ -15,7 +15,6 @@ import Data.Char as C
 --message = "d2:xsl1:01:11:21:01:11:21:01:11:2e2:ysl1:01:01:01:11:11:11:21:21:2e2:vsl1:X1:O1:O1:X1:O1:X1:X1:X1:Oee"
 -- expectedFrom = ([0, 1, 2, 0, 1, 2, 0, 1, 2], [0, 0, 0, 1, 1, 1, 2, 2, 2], ['X', 'O', 'O', 'X', 'O', 'X', 'X', 'X', 'O'])
  
---  ([Int], [Int], [Char])
 parse :: String -> ([Int], [Int], [Char])
 parse message = 
     let
@@ -67,17 +66,3 @@ removeFiveChars a = L.drop 5 a
 parseChar :: (Char, Char, Char) -> Char
 parseChar ('1',':',a) = a
 parseChar _ = error "Incorrect parameter"
-
--- tempRepeat :: Int -> String -> String
--- repeatTemp times message = 
---     let
---         [] ++ parseChar 
-
--- parseInt :: String -> Int
--- parseInt ('i':t) =
---     let
---         prefix = L.takeWhile C.isDigit t
---     in
---         read prefix
-
--- parseInt _ = error "Not an integer"
