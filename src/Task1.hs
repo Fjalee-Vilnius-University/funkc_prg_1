60dc54d42c513756666a6bd55a181179f3b4c96c
Rytis Cepulis <cepRytis@gmail.com>
2020-09-27 22:58:04 +0300

    bencode xs ys vs to expectedForm

--- src/Task1.hs
+++ src/Task1.hs
@@ -14,18 +14,39 @@ import Data.Char as C
 
 --message = "d2:xsl1:01:11:21:01:11:21:01:11:2e2:ysl1:01:01:01:11:11:11:21:21:2e2:vsl1:X1:O1:O1:X1:O1:X1:X1:X1:Oee"
 -- expectedFrom = ([0, 1, 2, 0, 1, 2, 0, 1, 2], [0, 0, 0, 1, 1, 1, 2, 2, 2], ['X', 'O', 'O', 'X', 'O', 'X', 'X', 'X', 'O'])
-
-parse :: String -> [String]
+ 
+--  ([Int], [Int], [Char])
+parse :: String -> ([Int], [Int], [Char])
 parse message = 
     let
         fstChCut = L.drop 1 message
-        fromArrWithName = tupleToArr (splitStringInto3 fstChCut)
-        fromArr = map removeFiveChars fromArrWithName
-    in
-        fromArr
+        threeBenCodesWName = tupleToArr (splitStringInto3 fstChCut)
+        threeBenCodes = map removeFiveChars threeBenCodesWName
+        arraysOfBenCoddedChars = map splitStringEvery3 threeBenCodes
+        arrFromStrings = map benCodeArrToArr arraysOfBenCoddedChars
+        xs = stringToIntArray (arrFromStrings !! 0)
+        ys = stringToIntArray (arrFromStrings !! 1)
+        vs = arrFromStrings !! 2
 
-tupleToArr :: (String, String, String) -> [String]
+    in
+        (xs, ys, vs)
+
+stringToIntArray :: [Char] -> [Int]
+stringToIntArray [] = []
+stringToIntArray (h:t) = [digitToInt h] ++ stringToIntArray t
+
+benCodeArrToArr :: [(Char,Char,Char)] -> [Char]
+benCodeArrToArr [] = []
+benCodeArrToArr (h:t) = [parseChar h] ++ benCodeArrToArr t
+
+tupleToArr :: (a, a, a) -> [a]
 tupleToArr (a, b, c) = [a, b, c]
+
+splitStringEvery3 :: [a] -> [(a,a,a)]
+splitStringEvery3 ([]) = []
+splitStringEvery3 (_:[]) = []
+splitStringEvery3 (_:_:[]) = []
+splitStringEvery3 (h1:h2:h3:t) = [(h1, h2, h3)] ++ splitStringEvery3 t
 
 splitStringInto3 :: String -> (String, String, String)
 splitStringInto3 a =
@@ -43,8 +64,8 @@ splitStringInto3 a =
 removeFiveChars :: String -> String
 removeFiveChars a = L.drop 5 a
 
-parseChar :: String -> [Char]
-parseChar ('1':':':a) = a
+parseChar :: (Char, Char, Char) -> Char
+parseChar ('1',':',a) = a
 parseChar _ = error "Incorrect parameter"
 
 -- tempRepeat :: Int -> String -> String
