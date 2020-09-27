50d6c90c41df13c2b83325b12a5dcc990cc1a9b0
Rytis Cepulis <cepRytis@gmail.com>
2020-09-27 21:54:26 +0300

    cut name (not useful) from fromARR (as Ben code)

--- src/Task1.hs
+++ src/Task1.hs
@@ -14,16 +14,18 @@ import Data.Char as C
 
 --message = "d2:xsl1:01:11:21:01:11:21:01:11:2e2:ysl1:01:01:01:11:11:11:21:21:2e2:vsl1:X1:O1:O1:X1:O1:X1:X1:X1:Oee"
 -- expectedFrom = ([0, 1, 2, 0, 1, 2, 0, 1, 2], [0, 0, 0, 1, 1, 1, 2, 2, 2], ['X', 'O', 'O', 'X', 'O', 'X', 'X', 'X', 'O'])
---"1:01:11:21:01:11:21:01:11:2e2:ysl1:01:01:01:11:11:11:21:21:2e2:vsl1:X1:O1:O1:X1:O1:X1:X1:X1:Oee"
 
-parse :: String -> (String, String, String)
+parse :: String -> [String]
 parse message = 
     let
         fstChCut = L.drop 1 message
-        fromArr = splitStringInto3 fstChCut
+        fromArrWithName = tupleToArr (splitStringInto3 fstChCut)
+        fromArr = map removeFiveChars fromArrWithName
     in
         fromArr
 
+tupleToArr :: (String, String, String) -> [String]
+tupleToArr (a, b, c) = [a, b, c]
 
 splitStringInto3 :: String -> (String, String, String)
 splitStringInto3 a =
@@ -38,13 +40,12 @@ splitStringInto3 a =
     in
         tuple
 
+removeFiveChars :: String -> String
+removeFiveChars a = L.drop 5 a
 
 parseChar :: String -> [Char]
 parseChar ('1':':':a) = a
 parseChar _ = error "Incorrect parameter"
-
--- removeSixChars :: String -> String
--- removeSixChars a = L.drop 6 a
 
 -- tempRepeat :: Int -> String -> String
 -- repeatTemp times message = 
