deedb6cb1c878223380c0941cfe83c11ad292d4f
Rytis Cepulis <cepRytis@gmail.com>
2020-09-27 17:56:22 +0300

    starting to build parse function

--- src/Task1.hs
+++ src/Task1.hs
@@ -1,4 +1,4 @@
-import Task1Message
+import Task1Message as M
 import Data.List as L
 import Data.Char as C
 
@@ -14,9 +14,10 @@ import Data.Char as C
 
 --message = "d2:xsl1:01:11:21:01:11:21:01:11:2e2:ysl1:01:01:01:11:11:11:21:21:2e2:vsl1:X1:O1:O1:X1:O1:X1:X1:X1:Oee"
 -- expectedFrom = ([0, 1, 2, 0, 1, 2, 0, 1, 2], [0, 0, 0, 1, 1, 1, 2, 2, 2], ['X', 'O', 'O', 'X', 'O', 'X', 'X', 'X', 'O'])
+--"1:01:11:21:01:11:21:01:11:2e2:ysl1:01:01:01:11:11:11:21:21:2e2:vsl1:X1:O1:O1:X1:O1:X1:X1:X1:Oee"
 
 parse :: String -> String
-parse message = message
+parse message = removeSixChars message
 
 -- parseString :: String -> String
 -- parseString = 
@@ -27,6 +28,11 @@ removeSixChars a = L.drop 6 a
 parseChar :: String -> [Char]
 parseChar ('1':':':a) = a
 parseChar _ = error "Incorrect parameter"
+
+-- tempRepeat :: Int -> String -> String
+-- repeatTemp times message = 
+--     let
+--         [] ++ parseChar 
 
 -- parseInt :: String -> Int
 -- parseInt ('i':t) =
